46fe26a5151dda0e2ff448cd5ee4b275028719dd
Maksym Lavryshchev <maksikxx@ukr.net>
2025-11-26 18:29:48 +0200

    Delete rpmbuild directory

--- rpmbuild/SOURCES/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Counting the number of files in the etc directory..."
-count=0
-for f in /etc/*; do
-	if [ -f "$f" ]; then
-		count=$((count+1))
-	fi
-done
-echo "Number of files in the etc directory: $count"
